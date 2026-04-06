3f4d68810b28ac2c9633ee00481ab63b6eae9abd
Marky0808 <todikashmangust@gmail.com>
2026-04-07 00:07:30 +0300

    Add files via upload

--- oop1.cs
+++ oop1.cs
@@ -21,6 +21,33 @@ public class oop1
         
         public decimal Cost => _cost;
         public static int TotalMembershipsCreated => _totalMembershipsCreated;
-
         
+        public virtual string GetInfo()
+        {
+            return $"Код: {_code}, Власник: {_ownerName}, Вартість: {_cost} грн";
+        }
+        
+        public bool IsExpensive()
+        {
+            return _cost > 1500;
+        }
+    }
+    
+    public class SingleVisit : Membership
+    {
+        private DateTime _visitDate;
+
+        public SingleVisit(string code, string ownerName, decimal cost, DateTime visitDate) 
+            : base(code, ownerName, cost)
+        {
+            _visitDate = visitDate;
+        }
+        
+        public override string GetInfo()
+        {
+            return base.GetInfo() + $", Дата відвідування: {_visitDate.ToShortDateString()}";
+        }
+    }
+
+   
 }
