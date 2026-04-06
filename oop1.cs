bd5ec79de30005ed566f2d1639381c21e2cc4c0e
Marky0808 <todikashmangust@gmail.com>
2026-04-06 23:59:26 +0300

    Add files via upload

--- oop1.cs
+++ oop1.cs
@@ -2,5 +2,25 @@ namespace Lab4;
 
 public class oop1
 {
-    
+    public class Membership
+    {
+        private readonly string _code;
+        private string _ownerName;
+        private decimal _cost;
+        
+        private static int _totalMembershipsCreated = 0;
+        
+        public Membership(string code, string ownerName, decimal cost)
+        {
+            _code = code;
+            _ownerName = ownerName;
+            _cost = cost;
+            
+            _totalMembershipsCreated++;
+        }
+        
+        public decimal Cost => _cost;
+        public static int TotalMembershipsCreated => _totalMembershipsCreated;
+
+        
 }
